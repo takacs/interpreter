faf7b6127e9017eac2df403bab8fe6a12cdb8bcd
takacs <davidetkcs@gmail.com>
2023-08-31 17:32:22 +0200

    Add errors to parsing

--- parser/parser.go
+++ parser/parser.go
@@ -4,22 +4,38 @@ import (
     "monkey/ast"
     "monkey/lexer"
     "monkey/token"
+
+    "fmt"
 )
 
 type Parser struct {
     l *lexer.Lexer
+
+    errors []string
 
     curToken token.Token
     peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-    p := &Parser{l: l}
+    p := &Parser{
+        l: l,
+        errors: []string{},
+    }
 
     p.nextToken()
     p.nextToken()
 
     return p
+}
+
+func (p *Parser) Errors() []string {
+    return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+    msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+    p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -60,6 +76,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
     stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 
+    
+    if !p.expectPeek(token.ASSIGN) {
+        return nil
+    }
+
     for !p.curTokenIs(token.SEMICOLON){
         p.nextToken()
     }
@@ -80,6 +101,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
         p.nextToken()
         return true
     } else {
+        p.peekError(t)
         return false
     }
 }
